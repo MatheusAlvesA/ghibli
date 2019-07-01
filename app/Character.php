130f3e0c88694893758d93932152180c62034b97
Matheus Alves de Andrade <matheuseejam@gmail.com>
2019-07-01 20:26:27 -0300

    Models createds

--- app/Character.php
+++ app/Character.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Character extends Model
 {
-    //
+	protected $table = 'characteres';
+
+	protected $primaryKey = 'id';  // The primary key on DB
+	public $incrementing = false;  // This key is not auto incrementing
+	protected $keyType = 'string'; // The type of the primary key is string
+
+	protected $fillable = ['id', 'name', 'age', 'eye_color', 'hair_color'];
 }
--- app/Movies.php
+++ app/Movies.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Movie extends Model
 {
-    //
+	protected $table = 'movies';
+
+	protected $primaryKey = 'id';  // The primary key on DB
+	public $incrementing = false;  // This key is not auto incrementing
+	protected $keyType = 'string'; // The type of the primary key is string
+
+	protected $fillable = ['id', 'name', 'description', 'director', 'producer', 'year', 'rtrate'];
 }
