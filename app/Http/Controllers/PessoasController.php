d54e80703a1eb00f32897eb95f4b7891c613ad58
Matheus Alves de Andrade <matheuseejam@gmail.com>
2019-07-02 09:42:59 -0300

    First version of controller

--- app/Http/Controllers/PessoasController.php
+++ app/Http/Controllers/PessoasController.php
@@ -2,19 +2,53 @@
 
 namespace App\Http\Controllers;
 
-use App\Model\Character;
-use App\Http\Resources\Pessoas;
 use Illuminate\Routing\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use DB;
 
 class PessoasController extends Controller
 {
     /**
      * Show all the characters and movies
-     *
-     * @return Resource
      */
-    public function show()
+    public function show(Request $r)
     {
-        return Pessoas::collection(Character::all());
-    }
+		$fmt = $r->query('fmt');
+		switch ($fmt) {
+			case 'json':
+				return response($this->getJoinedData()->get(), 200);
+				break;
+			case 'csv':
+				return response('In development', 200)
+						->header('Content-Type', 'text/plain');
+				break;
+			case 'html':
+				return response('In development', 200)
+						->header('Content-Type', 'text/plain');
+				break;
+
+			default:
+				return response('Erro: insert the format (fmt=[json, csv, html])', 400)
+						->header('Content-Type', 'text/plain');
+				break;
+		}
+
+	}
+	
+	/**
+     * Gets and joins all character data and your movies
+     */
+	private function getJoinedData() {
+		return DB::table('characters')
+		->join('movies_characters', 'characters.id', '=', 'movies_characters.character_id')
+		->join('movies', 'movies.id', '=', 'movies_characters.movie_id')
+		->select(
+					'characters.name',
+					'characters.age',
+					'movies.name as movieName',
+					'movies.year as movieYear',
+					'movies.rtrate as rtScore'
+		);
+	}
 }
